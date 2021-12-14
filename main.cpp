cd6981bc885b5069bcee04dd3a274cffe1ee830d
anishg555 <93986801+anishg555@users.noreply.github.com>
2021-12-14 12:56:15 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 //  Written by Anish Gautam (P2657398)
 
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 
 using namespace std;
 
 char DisplayMenu(); //shows menu
 void MenuOptions(char menuInput); //sorts the input
-void MainGameInput();
-
-int PlayerInput();
-int CompInput();
-void CheckResult(int playerIn, int compIn);
-void GameInfo(); //information about the game
+void MainGameInput(); //combines the following instructions into one to make int main neater
+void InitialiseGame(); // sets up the game layout
 void DisplayGameOptions(); //displays the options a user can put in
-void InitialiseGame();
-void QuitOptions();
+int PlayerInput(); //allows player to select an option
+int ShowPlayerInput(int playerIn);
+int CompInput(); //allows randomised computer input
+int ShowCompInput(int compInput); //displays computer input
+void CheckResult(int playerIn, int compIn); //sorts and displays results
+void GameEnd(); //allows player to return or continue
+void GameInfo(); //instructions
+void QuitOptions(); //allows user to quit
 
 bool gameActive = false; //so the game can be replayed
-bool goodInput = false; //allows return to menu
+bool goodInput = false;
 
-char playAgain;
+bool gameResult; //for the array later on
+char continueGame;
 
-
-bool results[5][5] = //rock, paper, scis, lzrd, spock - player
-{					{true, true, false, false, true}, //rock - comp
-					{false, true, true, true, false}, //paper
-					{true, false, true, false, true}, //scissors
-					{true, false, true, true, false}, //lizard
-					{false, true, false, true, true}, }; //spock
-
+//the array:
+bool results[5][5] =    //rock, paper, scis, lzrd, spock - player
+{						{true, true, false, false, true}, //rock - comp
+						{false, true, true, true, false}, //paper
+						{true, false, true, false, true}, //scissors
+						{true, false, true, true, false}, //lizard
+						{false, true, false, true, true}, }; //spock
 
 int main()
 {
@@ -59,7 +60,7 @@ char DisplayMenu()
 	cout << "||----------------------------------||" << endl;
 	cout << "|| [P] Start game                   ||" << endl;
 	cout << "||----------------------------------||" << endl;
-	cout << "|| [I] Game info                    ||" << endl;
+	cout << "|| [I] Game information             ||" << endl;
 	cout << "||----------------------------------||" << endl;
 	cout << "|| [Q] Exit game                    ||" << endl;
 	cout << "||----------------------------------||" << endl;
@@ -69,18 +70,17 @@ char DisplayMenu()
 	cout << "Please enter the command you would like to use: ";
 	cin >> menuInput;
 
-	return menuInput;
+	return menuInput; //for the switch statement in the next variable
 
 }
 
 void MenuOptions(char menuInput)
 {
-
 	switch (menuInput) {
 	case 'P':
 	case 'p':
-		goodInput = true;
-		PlayerInput();
+		goodInput = true; //having good input as true means the menu doesn't continue to show
+		MainGameInput();
 		break;
 	case 'I':
 	case 'i':
@@ -95,7 +95,7 @@ void MenuOptions(char menuInput)
 	default:
 		cout << "Invalid Input! ";
 		system("pause");
-		goodInput = false;
+		goodInput = false; //returns to menu
 		break;
 	}
 }
@@ -103,60 +103,6 @@ void MenuOptions(char menuInput)
 void MainGameInput()
 {
 	CheckResult(CompInput(), PlayerInput());
-}
-
-int PlayerInput()
-{
-	int playerIn = 0;
-
-	InitialiseGame();
-
-	cout << "Choose your move: ";
-	cin >> playerIn;
-
-	if (playerIn < 0 || playerIn > 4)
-	{
-		cout << "Invalid Input! ";
-		system("pause");
-		PlayerInput();
-	}
-	
-	return playerIn;
-}
-
-int CompInput()
-{
-	int compIn = 0;
-
-	compIn = rand() % 4;
-
-	return compIn;
-}
-
-void CheckResult(int playerIn, int compIn)
-{
-	//prevent ties
-
-	results[5][5] = results[playerIn][compIn];
-
-	if (results[5][5] == true)
-	{
-		if(playerIn = compIn)
-		{
-			cout << "Computer has rolled " << compIn << "!" << endl;
-			cout << "It's a draw!" << endl;
-			cout << "Play again [Y/N]? ";
-			cin >> playAgain;
-		}
-	}
-	else if (results[5][5] == false)
-	{
-		cout << "Computer has rolled " << compIn << "!" << endl;
-		cout << "Computer wins!" << endl;
-		cout << "Play again [Y/N]? ";
-		cin >> playAgain;
-	}
-	
 }
 
 void InitialiseGame()
@@ -179,13 +125,167 @@ void DisplayGameOptions()
 
 }
 
-void QuitOptions()
+int PlayerInput()
 {
-	goodInput = true;
+	int playerIn = 0;
 
-	cout << "Thank you for playing. ";
-	system("pause");
-	return;
+	InitialiseGame();
+
+	cout << "Choose your move: ";
+	cin >> playerIn;
+
+	if (playerIn > 4 || playerIn < 0)
+	{
+		// prevents bad numerical input
+
+		cout << "Invalid Input!" << endl;
+		system("pause");
+		PlayerInput();
+	}
+
+	if (!playerIn)
+	{
+		if (playerIn == 0) {
+			return playerIn;
+		}
+		else {
+			cout << "Invalid Input! ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); //this deletes the input, and prevents it from being entered
+
+			system("pause");
+			PlayerInput();
+		}
+	}
+
+	ShowPlayerInput(playerIn);
+
+	return playerIn;
+}
+
+int ShowPlayerInput(int playerIn)
+{
+	if (playerIn == 0)
+	{
+		cout << "You have chosen rock!" << endl;
+		return playerIn;
+	}
+	else
+	{
+		switch (playerIn) {
+		case 1:
+			cout << "You have chosen paper!" << endl;
+		break;
+		case 2:
+			cout << "You have chosen scissors!" << endl;
+		break;
+		case 3:
+			cout << "You have chosen lizard!" << endl;
+		break;
+		case 4:
+			cout << "You have chosen Spock!" << endl;
+		break;
+		default:
+			cout << "Invalid Input! " << endl;
+			system("pause");
+			PlayerInput();
+			break;
+		}
+	}
+
+	return playerIn;
+}
+
+int CompInput()
+{
+	int compIn = 0; //initialises variable
+
+	compIn = rand() % 5; //random number selection between 0-4, for use in the array
+
+	ShowCompInput(compIn);
+
+	return compIn;
+	
+}
+
+int ShowCompInput(int compIn)
+{
+	//displays to the user what the computer has chosen so they know how they won/lost
+
+	switch (compIn) {
+	case 0:
+		cout << "Computer has used rock!" << endl;
+		break;
+	case 1:
+		cout << "Computer has used paper!" << endl;
+		break;
+	case 2:
+		cout << "Computer has used scissors!" << endl;
+		break;
+	case 3:
+		cout << "Computer has used lizard!" << endl;
+		break;
+	case 4:
+		cout << "Computer has used Spock!" << endl;
+		break;
+	default:
+		break;
+	}
+	return compIn;
+}
+
+void CheckResult(int playerIn, int compIn)
+{
+	//prevent ties
+
+	if (playerIn == compIn)
+	{
+		cout << "Draw!" << endl;
+
+		GameEnd();
+
+	}
+	else
+	{
+		gameResult = results[playerIn][compIn];
+
+		if (gameResult == true)
+		{
+			cout << "Player wins!" << endl;
+
+			GameEnd();
+
+		}
+		else if (gameResult == false)
+		{
+			cout << "Computer wins!" << endl;
+
+			GameEnd();
+
+		}
+	}
+}
+
+void GameEnd()
+{
+	cout << "Play again [Y/N]? ";
+	cin >> continueGame;
+
+	switch (continueGame) {
+	case 'Y':
+	case 'y':
+		MainGameInput(); //continues game
+		break;
+	case 'N':
+	case 'n':
+		gameActive = false;
+		goodInput = false; //returns to menu
+		break;
+	default:
+		cout << "Invalid Input! ";
+		GameEnd(); //resets game end function
+		break;
+	}
 }
 
 void GameInfo()
@@ -207,5 +307,14 @@ void GameInfo()
 
 	goodInput = false;
 
+	return;
+}
+
+void QuitOptions()
+{
+	goodInput = true; //prevents menu from showing
+
+	cout << "Thank you for playing. ";
+	system("pause");
 	return;
 }
